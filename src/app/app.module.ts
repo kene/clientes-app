6281ae4168f1a3748b96809b0c5904d9c6ab19cc
kene <kene.prz.btz@gmail.com>
2020-10-18 22:24:34 -0500

    se agrega el httpClientModule y se configura en el importa para poder hacer las peticiones al servidor remoto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { ClientesComponent } from './clientes/clientes.component';
 import { DirectivaComponent } from './directiva/directiva.component';
 import { ClienteService } from './clientes/cliente.service';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 
 const routes: Routes = [ 
     {path: '', redirectTo: '/clientes', pathMatch: 'full'},
@@ -27,6 +27,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     RouterModule.forRoot(routes)
   ],
   providers: [ClienteService],
